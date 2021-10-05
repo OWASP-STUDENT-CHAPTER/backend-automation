dc39b2296f82983e99a92dd2e8e37908f22b784d
shivamdargan <darganshivam2001@gmail.com>
2021-10-06 03:01:22 +0530

    Added Basic Typescript Template With Short touch And FS operations For NPM Package

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-exports.printMsg = function() {
-    console.log("This is a message from Backend Automation NPM Package !");
-  }
+var touch = require("touch");
+var fs = require('fs');
+var content = "This is is the First Line Inside The Backend Automation Text File";
+touch('backend-automation.txt', function () {
+    console.log("An Automated Backend Text File has been created !");
+    fs.writeFile('./backend-automation.txt', content, 'utf8', function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+});
+exports.printMsg = function () {
+    console.log("This is a message from Backend Automation NPM Package and Basic Typescript Enviroment Setup !");
+};
